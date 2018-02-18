8a53c97a6c7a0c62c8682a69d9d008b638e6d13f
Christian Duvholt <christian@duvholt.net>
2018-02-18 14:56:28 +0100

    Rosenbrock plotter with static solutions

---
+++ plotting/plot.py
@@ -0,0 +1,50 @@
+# Rosenbrock variables
+a = 1.0
+b = 100.0
+
+def rosenbrock(x,y):
+      return (a-x)**2 + b* ((y-x**2))**2
+
+    
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plot
+import numpy as np
+
+fig = plot.figure()
+ax = fig.gca(projection='3d')
+
+
+solutions = np.array([
+    [1, 1],
+    [1.5, -1.3],
+    [1.5, -0.5],
+    [-2.0, 2.3],
+    [1.4, 1],
+])
+
+s = 0.25   # Try s=1, 0.25, 0.1, or 0.05
+max_x, max_y = solutions.max(axis=0)
+min_x, min_y = solutions.min(axis=0)
+padding = 0.5
+X = np.arange(min_x - padding, max_x + padding + s, s)   #Could use linspace instead if dividing
+Y = np.arange(min_y - padding, max_y + padding + s, s)  # evenly instead of stepping...
+X, Y = np.meshgrid(X, Y)
+    
+
+def plot_rosenbrock():
+    Z = (a - X)**2 + b * (Y-X*X)**2
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)  #Try coolwarm vs jet
+
+def plot_solution(x, y, iteration):
+    z = rosenbrock(x, y)
+    ax.plot([x], [y], [z], 'x', mew=1, markersize=5, color='red', label='1')
+    ax.text(x, y, z, iteration, color='#eeeeee')
+
+plot_rosenbrock()
+
+for iteration, solution in enumerate(solutions):
+    plot_solution(*solution, iteration)
+
+plot.show()
