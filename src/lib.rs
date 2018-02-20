8c23967b9409084e5928dbb30a1bfb2425d07a9a
Christian Duvholt <christian@duvholt.net>
2018-02-20 13:20:38 +0100

    Move test functions to new module

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use std::io::prelude::*;
 
 use rand::distributions::{IndependentSample, Range};
 use rand::{thread_rng, Rng};
-use std::f64::consts;
+mod test_functions;
 
 pub struct Config {
     pub start_t: f64,
@@ -70,7 +70,7 @@ pub struct Solution {
 
 impl Solution {
     pub fn new(x: f64, y: f64) -> Solution {
-        let fitness = ackley(x, y);
+        let fitness = test_functions::ackley(x, y);
         Solution {
             x,
             y,
@@ -85,36 +85,6 @@ impl Clone for Solution {
     fn clone(&self) -> Solution {
         *self
     }
-}
-
-fn ackley(x: f64, y: f64) -> f64 {
-    let a = 20.0;
-    let b = 0.2;
-    let c = 2.0 * consts::PI;
-    return -a *
-        (-b * (0.5 * (x.powf(2.0) + y.powf(2.0))).sqrt()).exp()
-        - (0.5 * ((c * x).cos() + (c * y).cos())).exp()
-        + a + consts::E
-}
-
-fn rosenbrock(x: f64, y: f64) -> f64 {
-    let a = 1.0;
-    let b = 100.0;
-    (a - x).powf(2.0) + b * (y - x.powf(2.0)).powf(2.0)
-}
-
-fn zakharov(x: f64, y: f64) -> f64 {
-    let mut sum1 = 0.0;
-    let mut sum2 = 0.0;
-
-    let dimensions = [x, y];
-    let mut i = 1.0;
-    dimensions.iter().for_each(|xi| {
-        sum1 = sum1 + xi.powf(2.0);
-        sum2 = sum2 + 0.5 * i * xi;
-        i += 1.0;
-    });
-    return sum1 + sum2.powf(2.0) + sum2.powf(4.0);
 }
 
 fn random_solution(config: &Config) -> Solution {
@@ -177,36 +147,6 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    #[test]
-    fn rosenbrock_optimum() {
-        assert_eq!(0.0, rosenbrock(1.0, 1.0));
-    }
-
-    #[test]
-    fn rosenbrock_not_optimum() {
-        assert_ne!(0.0, rosenbrock(3.0, 2.0));
-    }
-
-    #[test]
-    fn zakharov_optimum() {
-        assert_eq!(0.0, zakharov(0.0, 0.0));
-    }
-
-    #[test]
-    fn zakharov_not_optimum() {
-        assert_ne!(0.0, zakharov(2.0, -1.3));
-    }
-
-    #[test]
-    fn ackley_optimum() {
-        // TODO: Check if this is actually the minimum
-        assert_eq!(0.0000000000000004440892098500626, ackley(0.0, 0.0));
-    }
-
-    #[test]
-    fn ackley_not_optimum() {
-        assert_ne!(0.0, ackley(2.0, -1.3));
-    }
 
     #[test]
     fn generates_neighbour() {
---
+++ src/test_functions.rs
@@ -0,0 +1,68 @@
+use std::f64::consts;
+
+pub fn ackley(x: f64, y: f64) -> f64 {
+    let a = 20.0;
+    let b = 0.2;
+    let c = 2.0 * consts::PI;
+    return -a *
+        (-b * (0.5 * (x.powf(2.0) + y.powf(2.0))).sqrt()).exp()
+        - (0.5 * ((c * x).cos() + (c * y).cos())).exp()
+        + a + consts::E
+}
+
+pub fn rosenbrock(x: f64, y: f64) -> f64 {
+    let a = 1.0;
+    let b = 100.0;
+    (a - x).powf(2.0) + b * (y - x.powf(2.0)).powf(2.0)
+}
+
+pub fn zakharov(x: f64, y: f64) -> f64 {
+    let mut sum1 = 0.0;
+    let mut sum2 = 0.0;
+
+    let dimensions = [x, y];
+    let mut i = 1.0;
+    dimensions.iter().for_each(|xi| {
+        sum1 = sum1 + xi.powf(2.0);
+        sum2 = sum2 + 0.5 * i * xi;
+        i += 1.0;
+    });
+    return sum1 + sum2.powf(2.0) + sum2.powf(4.0);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rosenbrock_optimum() {
+        assert_eq!(0.0, rosenbrock(1.0, 1.0));
+    }
+
+    #[test]
+    fn rosenbrock_not_optimum() {
+        assert_ne!(0.0, rosenbrock(3.0, 2.0));
+    }
+
+    #[test]
+    fn zakharov_optimum() {
+        assert_eq!(0.0, zakharov(0.0, 0.0));
+    }
+
+    #[test]
+    fn zakharov_not_optimum() {
+        assert_ne!(0.0, zakharov(2.0, -1.3));
+    }
+
+    #[test]
+    fn ackley_optimum() {
+        // TODO: Check if this is actually the minimum
+        assert_eq!(0.0000000000000004440892098500626, ackley(0.0, 0.0));
+    }
+
+    #[test]
+    fn ackley_not_optimum() {
+        assert_ne!(0.0, ackley(2.0, -1.3));
+    }
+}
