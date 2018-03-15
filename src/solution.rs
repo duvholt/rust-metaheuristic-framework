67a12fe7e3c7169b5c103afb43635e43133e69e4
Christian Duvholt <christian@duvholt.net>
2018-03-15 19:02:08 +0100

    Rename Solution to SolutionJSON

--- src/solution.rs
+++ src/solution.rs
@@ -1,16 +1,17 @@
 #[derive(Serialize)]
 pub struct Solutions {
-    pub solutions: Vec<Solution>,
+    pub solutions: Vec<SolutionJSON>,
     pub test_function: String,
 }
 
 #[derive(Serialize, Clone)]
-pub struct Solution {
+pub struct SolutionJSON {
     pub x: Vec<f64>,
     pub fitness: f64,
 }
-impl Solution {
-    pub fn new(x: Vec<f64>, fitness: f64) -> Solution {
-        Solution { x, fitness }
+
+impl SolutionJSON {
+    pub fn new(x: Vec<f64>, fitness: f64) -> SolutionJSON {
+        SolutionJSON { x, fitness }
     }
 }
