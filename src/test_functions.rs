e96b4e0d7e2ebc1b2d4ebd69cd7eded0fa43e30e
Christian Duvholt <christian@duvholt.net>
2018-03-20 02:14:09 +0100

    Add proper support for selecting multi objective test functions

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -1,5 +1,29 @@
 use std::f64::consts;
 
+pub type SingleTestFunction = Fn(&Vec<f64>) -> f64;
+pub type MultiTestFunction = Fn(&Vec<f64>) -> Vec<f64>;
+
+type SingleTestFunctionVar = fn(&Vec<f64>) -> f64;
+type MultiTestFunctionVar = fn(&Vec<f64>) -> Vec<f64>;
+pub enum TestFunctionVar {
+    Single(SingleTestFunctionVar),
+    Multi(MultiTestFunctionVar),
+}
+
+pub fn get_single(test_function_var: TestFunctionVar) -> SingleTestFunctionVar {
+    match test_function_var {
+        TestFunctionVar::Single(f) => f,
+        _ => panic!("Algorithm only supports single objective functions"),
+    }
+}
+
+pub fn get_multi(test_function_var: TestFunctionVar) -> MultiTestFunctionVar {
+    match test_function_var {
+        TestFunctionVar::Multi(f) => f,
+        _ => panic!("Algorithm only supports single objective functions"),
+    }
+}
+
 pub fn ackley(x: &Vec<f64>) -> f64 {
     let a = 20.0;
     let b = 0.2;
