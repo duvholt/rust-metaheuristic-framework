e4cd177721ef4930de14d489624cad8e67243fe6
Eirik <eiribert@stud.ntnu.no>
2018-03-28 15:57:06 +0200

    added zdt3

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -95,6 +95,14 @@ pub fn zdt2(x: &Vec<f64>) -> Vec<f64> {
     let f2 = g * h;
     vec![f1, f2]
 }
+pub fn zdt3(x: &Vec<f64>) -> Vec<f64> {
+    let f1 = x[0];
+    let sum: f64 = x.iter().skip(1).sum();
+    let g = 1.0 + (9.0 / (x.len() as f64 - 1.0)) * sum;
+    let h = 1.0 - (f1 / g).sqrt() - (f1 / g) * (10.0 * consts::PI * f1).sin();
+    let f2 = g * h;
+    vec![f1, f2]
+}
 
 pub fn axis_parallel_hyper_ellipsoid(x: &Vec<f64>) -> f64 {
     (0..x.len()).map(|i| (i as f64 + 1.0) * x[i].powi(2)).sum()
