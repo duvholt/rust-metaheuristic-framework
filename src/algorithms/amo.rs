3e7b3567bafca917ee2da24b2fdfb241ad843275
xcr <xecor2k@hotmail.com>
2018-04-05 18:27:38 +0200

    Implement animal replacement for amo

--- src/algorithms/amo.rs
+++ src/algorithms/amo.rs
@@ -62,12 +62,40 @@ fn animal_migration(
     find_best_solutions(population, moved_animals)
 }
 
+fn animal_replacement(
+    mut population: Vec<Animal>,
+    best_animal: Animal,
+    mut rng: impl Rng,
+    fitness_evaluator: &FitnessEvaluator<f64>,
+    config: &Config,
+) -> Vec<Animal> {
+    sort_solutions_by_fitness(&mut population);
+    let new_population: Vec<Animal> = (0..population.len())
+        .map(|i| {
+            let mut animal = population[i].clone();
+            for d in 0..config.dimension {
+                let r = get_two_unique_numbers(population.len(), population.len(), &mut rng);
+                if rng.next_f64() > (population.len() - i) as f64 / population.len() as f64 {
+                    let r1 = population[r.0].position[d];
+                    let r2 = population[r.1].position[d];
+                    let best = best_animal.position[d];
+                    let current = population[i].position[d];
+                    animal.position[d] =
+                        r1 + rng.next_f64() * (best - current) + rng.next_f64() * (r2 - current);
+                }
+            }
+            animal.fitness = fitness_evaluator.calculate_fitness(&animal.position);
+            animal
+        })
+        .collect();
+    find_best_solutions(population, new_population)
+}
+
 fn find_best_solutions(old_population: Vec<Animal>, new_population: Vec<Animal>) -> Vec<Animal> {
     old_population
         .into_iter()
         .zip(new_population)
         .map(|(old, new)| {
-            println!("old {:?}    new {:?}", old, new);
             if old.fitness > new.fitness {
                 new
             } else {
@@ -129,12 +157,16 @@ mod tests {
         }
     }
 
+    fn create_seedable_rng() -> StdRng {
+        let seed: &[_] = &[1, 2, 3, 4];
+        SeedableRng::from_seed(seed)
+    }
+
     #[test]
     fn get_two_unique_numbers_test() {
         let i = 2;
         let length = 3;
-        let seed: &[_] = &[1, 2, 3, 4];
-        let mut rng: StdRng = SeedableRng::from_seed(seed);
+        let mut rng = create_seedable_rng();
         assert_eq!(get_two_unique_numbers(i, length, &mut rng), (1, 0));
         assert_eq!(get_two_unique_numbers(i, length, &mut rng), (0, 1));
         assert_eq!(get_two_unique_numbers(i, length, &mut rng), (0, 1));
@@ -145,6 +177,42 @@ mod tests {
         assert_eq!(get_random_neighbor_index(5, 50), 5);
         assert_eq!(get_random_neighbor_index(-2, 50), 48);
         assert_eq!(get_random_neighbor_index(52, 50), 2);
+    }
+
+    #[test]
+    fn animal_replacement_test() {
+        let config = create_config();
+        let sampler = create_sampler();
+        let fitness_evaluator = &create_evaluator(&sampler);
+        let best = Animal {
+            position: vec![0.1, 0.2],
+            fitness: 0.3,
+        };
+        let population = vec![
+            Animal {
+                position: vec![1.0, 2.0],
+                fitness: 3.0,
+            },
+            Animal {
+                position: vec![2.0, 2.1],
+                fitness: 4.1,
+            },
+            Animal {
+                position: vec![3.0, 2.3],
+                fitness: 5.3,
+            },
+            Animal {
+                position: vec![4.0, 2.6],
+                fitness: 6.6,
+            },
+        ];
+
+        let rng = create_seedable_rng();
+        let new_population = animal_replacement(population, best, rng, &fitness_evaluator, &config);
+        assert_eq!(new_population[0].fitness, 3.0);
+        assert_eq!(new_population[1].fitness, 4.1);
+        assert_eq!(new_population[2].fitness, 4.0448939501300325);
+        assert_eq!(new_population[3].fitness, 5.388251274328821);
     }
 
     #[test]
@@ -171,8 +239,7 @@ mod tests {
             },
         ];
 
-        let seed: &[_] = &[1, 2, 3, 4];
-        let mut rng: StdRng = SeedableRng::from_seed(seed);
+        let mut rng = create_seedable_rng();
         let next_generation = animal_migration(population, rng, &fitness_evaluator, &config);
         assert_eq!(next_generation[0].fitness, 1.0);
         assert_eq!(next_generation[1].fitness, 2.584863028248279);
