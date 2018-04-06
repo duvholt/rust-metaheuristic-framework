d6bd84ee85226719179555f7e3ce25494c1ef7b7
Christian Duvholt <christian@duvholt.net>
2018-04-07 01:26:19 +0200

    Implement moving towards safe place

--- src/position.rs
+++ src/position.rs
@@ -27,9 +27,27 @@ pub fn perpendicular_position(position: &Vec<f64>) -> Vec<f64> {
     perpendicular
 }
 
+pub fn euclidean_distance(a: &[f64], b: &[f64]) -> f64 {
+    a.iter()
+        .zip(b)
+        .map(|(a1, b1)| (b1 - a1).powi(2))
+        .sum::<f64>()
+        .sqrt()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn euclidean_distance_correct() {
+        let a = vec![1.0, 0.0];
+        let b = vec![4.0, 3.0];
+
+        let distance = euclidean_distance(&a, &b);
+
+        assert_eq!(distance, 18f64.sqrt());
+    }
 
     #[test]
     fn creates_random_solution() {
