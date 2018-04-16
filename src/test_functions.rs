948de243761204dfa0505272bc10b50962a27de5
Eirik <eiribert@stud.ntnu.no>
2018-04-16 12:42:11 +0200

    Add DTLZ4

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -152,6 +152,31 @@ pub fn dtlz3(x: &Vec<f64>) -> Vec<f64> {
     result
 }
 
+pub fn dtlz4(x: &Vec<f64>) -> Vec<f64> {
+    let mut result = vec![];
+    let m = 3;
+    let a = 100;
+    let k = x.len() - m + 1;
+    let g: f64 = (k..x.len()).map(|i| (x[i] - 0.5).powi(2)).sum();
+    //f_1
+    result.push(
+        (1.0 + g)
+            * (0..m - 1)
+                .map(|i| (x[i].powi(a) * consts::PI / 2.0).cos())
+                .product::<f64>(),
+    );
+    //f_2 to f_m-1
+    for i in 1..m - 1 {
+        let product: f64 = (0..m - (i + 1))
+            .map(|j| (x[j].powi(a) * consts::PI / 2.0).cos())
+            .product();
+        result.push((1.0 + g) * product * (x[m - (i + 1)].powi(a) * consts::PI / 2.0).sin());
+    }
+    //f_m
+    result.push((1.0 + g) * (x[0].powi(a) * consts::PI / 2.0).sin());
+    result
+}
+
 pub fn axis_parallel_hyper_ellipsoid(x: &Vec<f64>) -> f64 {
     (0..x.len()).map(|i| (i as f64 + 1.0) * x[i].powi(2)).sum()
 }
