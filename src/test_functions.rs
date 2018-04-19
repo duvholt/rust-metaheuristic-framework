f64c6b9355badf50212cdf6f88f2f7a8ef03e848
Eirik <eiribert@stud.ntnu.no>
2018-04-19 15:08:36 +0200

    Add dtlz4 tests

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -288,6 +288,33 @@ mod tests {
     }
 
     #[test]
+    fn dtlz4_optimum() {
+        let mut rng = thread_rng();
+        let objectives = 3;
+        for i in objectives..31 {
+            let mut vector = vec![0.5; i];
+            let k = i - objectives + 1;
+            for j in 0..i - k {
+                vector[j] = rng.next_f64();
+                let result = dtlz4(&vector);
+                assert_approx_eq!(result.iter().map(|i| i.powi(2)).sum::<f64>(), 1.0);
+            }
+        }
+    }
+
+    #[test]
+    fn dtlz4_not_optimum() {
+        let result = dtlz4(&vec![0.5, 0.5, 0.55, 0.5]);
+        assert_ne!(result.iter().map(|i| i.powi(2)).sum::<f64>(), 1.0);
+        let result = dtlz4(&vec![0.5, 0.5, 0.5, 0.8]);
+        assert_ne!(result.iter().map(|i| i.powi(2)).sum::<f64>(), 1.0);
+        let result = dtlz4(&vec![0.5, 0.5, 0.5, 0.6]);
+        assert_ne!(result.iter().map(|i| i.powi(2)).sum::<f64>(), 1.0);
+        let result = dtlz4(&vec![0.5, 0.5, 0.6, 0.6]);
+        assert_ne!(result.iter().map(|i| i.powi(2)).sum::<f64>(), 1.0);
+    }
+
+    #[test]
     fn rastrigin_optimum() {
         assert_eq!(rastrigin(&vec![0.0]), 0.0);
         assert_eq!(rastrigin(&vec![0.0, 0.0]), 0.0);
