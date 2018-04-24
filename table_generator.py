826c141251109f4cfa751c3c6a2eba18d05986e8
Eirik <eiribert@stud.ntnu.no>
2018-04-24 16:10:52 +0200

    Fix table_generator to work with python 3.5

--- table_generator.py
+++ table_generator.py
@@ -12,16 +12,16 @@ algorithms = {
     'ewa': {
         'p': 50
     },
-    # 'loa': {
-    #     'p': 150
-    # }
+    'loa': {
+        'p': 50
+    }
 }
 
 results = {}
 
 for name, parameters in algorithms.items():
-    command = f'cargo run --release -- -t cec2014 -r 25 -u 30 -d 30 -i 10000 -j -p {parameters["p"]} {name}'
-    json_output = json.loads(check_output(command.split()).split()[-1])
+    command = 'cargo run --release -- -t cec2014 -r 25 -u 30 -d 30 -i 10000 -j -p {} {}'.format(parameters['p'], name)
+    json_output = json.loads(check_output(command.split()).split()[-1].decode('utf-8'))
     for tf in json_output:
         if tf['test_function'] in results:
             results[tf['test_function']].append(tf)
@@ -29,7 +29,7 @@ for name, parameters in algorithms.items():
             results[tf['test_function']] = [tf]
 
 for test_function, alg_data in results.items():
-    table_data = f'{test_function} '
+    table_data = test_function
     for stat_data in alg_data:
-        table_data += f'& {stat_data["mean"]:.2E} & {stat_data["standard_deviation"]:.2E} '
+        table_data += ' & {:.2E} & {:.2E} '.format(stat_data["mean"], stat_data["standard_deviation"])
     print(table_data + '\\\\')
