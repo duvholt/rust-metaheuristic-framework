988c4124d7c6656c1f58da9aaf64c70e774ec454
Christian Duvholt <christian@duvholt.net>
2018-04-26 17:34:12 +0200

    Add support for dynamic parameters to table generator

--- table_generator.py
+++ table_generator.py
@@ -4,17 +4,35 @@ import json
 import sys
 
 default_parameters = {
-    'r': 25,  # Runs
-    'd': 30,  # Dimensions
-    'u': 100,  # Upper bound
-    'i': 10000,  # Iterations
-    'p': 50,  # Population
+    '-r': 1,  # Runs
+    '-d': 2,  # Dimensions
+    '-u': 100,  # Upper bound
+    '-i': 1000,  # Iterations
+    '-p': 50,  # Population
 }
 
 
-def run_algorithm(parameters, results):
-    command = 'cargo run --release -- -t cec2014 -r {r} -u {u} -d {d} -i {i} -j -p {p} {algorithm}'.format(
-        **parameters)
+def dict_to_params(d):
+    params = ''
+    for key, value in d.items():
+        params += ' {key} {value}'.format(key=key, value=value)
+    return params
+
+
+def specific_parameters(parameters):
+    if 'specific' in parameters:
+        algorithm_parameters = dict_to_params(parameters['specific'])
+        del parameters['specific']
+    else:
+        algorithm_parameters = ''
+    return algorithm_parameters
+
+
+def run_algorithm(algorithm, parameters, results):
+    algorithm_parameters = specific_parameters(parameters)
+    suite_parameters = dict_to_params(parameters)
+    command = 'cargo run --release -- -t single -j {suite_parameters} {algorithm} {algorithm_parameters}'.format(
+        algorithm_parameters=algorithm_parameters, suite_parameters=suite_parameters, algorithm=algorithm)
     json_output = json.loads(check_output(
         command.split()).split()[-1].decode('utf-8'))
     for tf in json_output:
@@ -25,31 +43,36 @@ def run_algorithm(parameters, results):
 
 
 def tune_algorithm():
-    algorithm = 'amo'
+    algorithm = 'ewa'
     configs = [
         {
-            'p': 25,
-            'i': 10000,
+            'specific': {
+                '-c': 0.9
+            }
+        },
+         {
+            'specific': {
+                '-c': 0.99
+            }
         },
         {
-            'p': 35,
-            'i': 10000,
+            '-u': 32,
+            '-l': -28,
         },
         {
-            'p': 50,
-            'i': 10000,
+            '-u': 32,
+            '-l': -28,
+             'specific': {
+                 '-c': 0.99
+             }
         },
-        {
-            'p': 75,
-            'i': 10000,
-        }
     ]
 
     results = OrderedDict()
 
     for config_parameters in configs:
-        parameters = {**default_parameters, **config_parameters, 'algorithm': algorithm}
-        run_algorithm(parameters, results)
+        parameters = {**default_parameters, **config_parameters}
+        run_algorithm(algorithm, parameters, results)
 
     generate_table(results)
 
@@ -57,7 +80,7 @@ def tune_algorithm():
 def algorithms():
     configs = [
         {'algorithm': 'amo'},
-        {'algorithm': 'da', 'p': 1},
+        {'algorithm': 'da', '-p': 1},
         {'algorithm': 'ewa'},
         {'algorithm': 'loa'},
     ]
@@ -65,8 +88,10 @@ def algorithms():
     results = OrderedDict()
 
     for config_parameters in configs:
+        algorithm = config_parameters['algorithm']
+        del config_parameters['algorithm']
         parameters = {**default_parameters, **config_parameters}
-        run_algorithm(parameters, results)
+        run_algorithm(algorithm, parameters, results)
 
     generate_table(results)
 
